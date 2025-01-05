2a67556fe13fdc1965929dccbf728a3f70d45150
Ebrahim Essa Ahmed <99309335+12288987@users.noreply.github.com>
2025-01-05 08:16:55 +0300

    banner

---
+++ netflix-clone/src/components/banner/Banner.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import './banner.css'
+import axios from '../../utils/axios'
+import request from '../../utils/request'
+
+const Banner = () => {
+    const [movie, setMovie] = useState({});
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const requeste = await axios.get(request.fetchNetflixOriginals)
+                console.log(requeste);
+                setMovie(requeste.data.results[
+                    Math.floor(Math.random() * requeste.data.results.length)
+                ]);
+            } catch (error) {
+                console.log("error",error);
+            }
+        })()
+    }, []);
+    function trancuate(str, n) {
+        return str?.length > n? str.substr(0, n-1) + '...': str;
+    };
+  return (
+    <div className='banner'
+    style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path
+        }")`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat"
+    }}
+    >
+        <div className='banner-contents'>
+            <h1 className='banner-title'>
+                {movie?.title||movie?.name
+||movie?.original_name
+}
+            </h1>
+            <div className='banner-buttons'>
+                <button className='banner-button play'>Play</button>
+                <button className='banner-button'>My List</button>
+            </div>
+            <h1 className='banner-description'>{trancuate(movie?.overview, 150)}</h1>
+        </div>
+        <div className='banner-fadeBottom'/>
+    </div>
+  )
+}
+
+export default Banner;
+
+// useEffect(() = {
+//     (async ()=> {
+//         try {
+//             const requeste = await axios.get(request.fetchNetflixOriginals)
+//             setMovie(request.data.results[
+//                 Math.floor(Math.random() * request.data.results.length)
+//             ]);
+//         } catch (error) {
+//             console.log("error", error);
+//         }
+//     })()
+// }, [])
--- netflix-clone/src/utils/request.js
+++ netflix-clone/src/utils/request.js
@@ -10,3 +10,5 @@ const requests = {
     fetchDocumentaryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99';`,
     fetchTvShows: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1';`
 }
+
+export default requests;
